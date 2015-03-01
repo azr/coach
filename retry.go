a0d9a973c8584c42075fa67d368381f80db1010e
Adrien Delorme <adrien.delorme@icloud.com>
2015-03-01 10:57:28 +0100

    doc(Retry): explain Retry more

--- retry.go
+++ retry.go
@@ -5,10 +5,12 @@ import (
 	"time"
 )
 
-// Retry if operation fails, wait for the duration returned by callback or exits if the error is too big.
-// Just return nil if operation is successfull.
+// Retry will execute operation, if operation returns an error,
+// time.Sleep for the duration returned by callback or exits if callback returns an error.
 //
 // This is pretty convenient to allow you to check the operation's error before retrying, if any.
+//
+// A negative or zero duration causes Sleep to return immediately.
 func Retry(operation func() error, callback func(error) (time.Duration, error)) error {
 	for err := operation(); err != nil; err = operation() {
 		if d, err := callback(err); err != nil {
